00ffcedd444a0cee9715ebb8e3a8631342fc4f38
GreatUstug <m.ustugoff@gmail.com>
2025-07-28 13:13:30 +0300

    files

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController
+{
+    #[Route('/', name: 'index')]
+    #[Route('/hello/{name}', name: 'index')]
+    
+    public function index(string $name): Response
+    {
+        return new Response("Hello, $name!");
+    }
+
+    #[Route('/simplicity', methods: ['GET'])]
+    public function simple(): Response
+    {
+        return new Response('Simple! Easy! Great!');
+    }
+}
+?>
